4ee20775004bfd52c96a2f2a48353b38dd7c8788
IsmaSEO <ismail.jelassi.dev@gmail.com>
2025-03-25 00:20:31 +0100

    Ajout du composant Footer

---
+++ hrnet/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>Copyright © 2025 WealthHealth</p>
+    </footer>
+  );
+};
+
+export default Footer;
